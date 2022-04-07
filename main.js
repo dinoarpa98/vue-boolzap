ba1fb3cccae418df6992ef68e805f5e428d02640
dinoarpa98 <dinoarpa98@gmail.com>
2022-04-07 09:25:39 +0200

    m4

--- main.js
+++ main.js
@@ -1,44 +1,45 @@
 var app = new Vue({
-    el: "#root",
-    
-    data: {
-       
-        indexContact: 0,
-        newMessageText: '',
+el: "#root",
 
-        profilo: {
-            name: 'Sofia',
-            img: '_io'
-        },
-        
-        //contacts[indexOggetto].messages.message
-        
-        contacts: [{
-                name: 'Michele',
-                avatar: '_1',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Hai portato a spasso il cane?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Ricordati di stendere i panni',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 16:15:22',
-                    message: 'Tutto fatto!',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Fabio',
-            avatar: '_2',
-            visible: true,
-            messages: [{
+data: {
+
+indexContact: 0,
+newMessageText: "",
+Search : "",
+
+profilo: {
+    name: 'Sofia',
+    img: '_io'
+},
+
+//contacts[indexOggetto].messages.message
+
+contacts: [{
+        name: 'Michele',
+        avatar: '_1',
+        visible: true,
+        messages: [{
+                date: '10/01/2020 15:30:55',
+                message: 'Hai portato a spasso il cane?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Ricordati di stendere i panni',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 16:15:22',
+                message: 'Tutto fatto!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true,
+        messages: [{
                 date: '20/03/2020 16:30:00',
                 message: 'Ciao come stai?',
                 status: 'sent'
@@ -60,43 +61,43 @@ var app = new Vue({
         avatar: '_3',
         visible: true,
         messages: [{
-            date: '28/03/2020 10:10:40',
-            message: 'La Marianna va in campagna',
-            status: 'received'
-        },
-        {
-            date: '28/03/2020 10:20:10',
-            message: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
-        },
-        {
-            date: '28/03/2020 16:15:22',
-            message: 'Ah scusa!',
-            status: 'received'
-                    }
-                ],
+                date: '28/03/2020 10:10:40',
+                message: 'La Marianna va in campagna',
+                status: 'received'
             },
             {
-                name: 'Alessandro B.',
-                avatar: '_4',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                }
-            ],
-        },
-        {
-            name: 'Alessandro L.',
-            avatar: '_5',
-            visible: true,
-            messages: [{
+                date: '28/03/2020 10:20:10',
+                message: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
+            },
+            {
+                date: '28/03/2020 16:15:22',
+                message: 'Ah scusa!',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Alessandro B.',
+        avatar: '_4',
+        visible: true,
+        messages: [{
+                date: '10/01/2020 15:30:55',
+                message: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+            }
+        ],
+    },
+    {
+        name: 'Alessandro L.',
+        avatar: '_5',
+        visible: true,
+        messages: [{
                 date: '10/01/2020 15:30:55',
                 message: 'Ricordati di chiamare la nonna',
                 status: 'sent'
@@ -107,33 +108,33 @@ var app = new Vue({
                 status: 'received'
             }
         ],
+    },
+    {
+        name: 'Claudia',
+        avatar: '_6',
+        visible: true,
+        messages: [{
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao Claudia, hai novità?',
+                status: 'sent'
             },
             {
-                name: 'Claudia',
-                avatar: '_6',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Ciao Claudia, hai novità?',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Non ancora',
-                    status: 'received'
-                },
-                {
-                    date: '10/01/2020 15:51:00',
-                    message: 'Nessuna nuova, buona nuova',
-                    status: 'sent'
-                }
-            ],
-        },
-        {
-            name: 'Federico',
-            avatar: '_7',
-            visible: true,
-            messages: [{
+                date: '10/01/2020 15:50:00',
+                message: 'Non ancora',
+                status: 'received'
+            },
+            {
+                date: '10/01/2020 15:51:00',
+                message: 'Nessuna nuova, buona nuova',
+                status: 'sent'
+            }
+        ],
+    },
+    {
+        name: 'Federico',
+        avatar: '_7',
+        visible: true,
+        messages: [{
                 date: '10/01/2020 15:30:55',
                 message: 'Fai gli auguri a Martina che è il suo compleanno!',
                 status: 'sent'
@@ -150,22 +151,22 @@ var app = new Vue({
         avatar: '_8',
         visible: true,
         messages: [{
-            date: '10/01/2020 15:30:55',
-            message: 'Ciao, andiamo a mangiare la pizza stasera?',
-            status: 'received'
-        },
-        {
-            date: '10/01/2020 15:50:00',
-            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-            status: 'sent'
-        },
-        {
-            date: '10/01/2020 15:51:00',
-            message: 'OK!!',
-            status: 'received'
-        }
-    ],
-},
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                status: 'received'
+            },
+            {
+                date: '10/01/2020 15:50:00',
+                message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                status: 'sent'
+            },
+            {
+                date: '10/01/2020 15:51:00',
+                message: 'OK!!',
+                status: 'received'
+            }
+        ],
+    },
 ],
 
 
@@ -173,11 +174,49 @@ var app = new Vue({
 
 //indexContact
 methods: {
-    
-    setIndexContact: function (index) {
-        console.log(index);
-        this.indexContact = index;
-        console.log(`l'indice è diventato ${this.indexContact}`)
+
+setIndexContact: function (index) {
+    console.log(index);
+    this.indexContact = index;
+    console.log(`l'indice è diventato ${this.indexContact}`)
+},
+
+newMessage: function () {
+    let Date = dayjs().format('DD/MM/YYYY');
+    let hour = dayjs().get('hour');
+    let minutes = dayjs().get('minutes');
+
+    let newMessageObj = {
+        date: `${Date} ${hour}:${minutes}`,
+        message: this.newMessageText,
+        status: 'sent'
+    }
+
+    let newMessageReceived = {
+        date: `${Date} ${hour}:${minutes}`,
+        message: 'ok',
+        status: 'received'
+    }
+
+    this.contacts[this.indexContact].messages.push(newMessageObj)
+
+    setTimeout(
+        () => {
+
+            this.contacts[this.indexContact].messages.push(newMessageReceived);
+        }, 1000
+    );
+},
+
+
+filterContacts: function (e) { // filtra contatti con input
+    let searchN = this.Search.toLowerCase();
+    let filterN = e.name.toLowerCase();
+    if (filterN.includes(searchN)) {
+        return true
     }
 }
+}
 })
+
+
